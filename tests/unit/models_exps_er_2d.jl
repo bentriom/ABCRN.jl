412e718b1c74a4129e1f05c134fe0a7ec7ef40df
Mahmoud Bentriou <mahmoud.bentriou@centralesupelec.fr>
2021-01-13 20:41:39 +0100

    add of R4 in models_exps_er_2d

--- tests/unit/models_exps_er_2d.jl
+++ tests/unit/models_exps_er_2d.jl
@@ -7,6 +7,7 @@ load_automaton("automaton_F")
 load_automaton("automaton_G")
 load_automaton("automaton_G_and_F")
 
+A_F_R4 = create_automaton_F(ER, 5.0, 15.0, 8.0, 10.0, :P)
 x1, x2, t1, t2 = 50.0, 100.0, 0.0, 0.8
 A_G_R5 = create_automaton_G(ER, x1, x2, t1, t2, :E) 
 x3, x4, t3, t4 = 30.0, 100.0, 0.8, 0.9
