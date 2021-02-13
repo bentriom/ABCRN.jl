a1786dc00dccb5ed2c8799d217829988d516f6fc
Mahmoud Bentriou <mahmoud.bentriou@centralesupelec.fr>
2021-02-13 23:45:07 +0100

    modify names of repressilator + small fix in plots of oscillatory trajectories

--- models/repressilator.jl
+++ models/repressilator.jl
@@ -1,17 +1,24 @@
 
 repressilator = @network_model begin
-    R1: (G1 => G1 + m1, α/(1+p3^n) + α0)
-    R2: (G2 => G2 + m2, α/(1+p1^n) + α0)
-    R3: (G3 => G3 + m3, α/(1+p2^n) + α0)
-    R4: (m1 => m1 + p1, β * m1)
-    R5: (m2 => m2 + p2, β * m2)
-    R6: (m3 => m3 + p3, β * m3)
-    R7: (m1 => 0, m1)
-    R8: (m2 => 0, m2)
-    R9: (m3 => 0, m3)
+    tr1: (G1 => G1 + mRNA1, α/(1+P3^n) + α0)
+    tr2: (G2 => G2 + mRNA2, α/(1+P1^n) + α0)
+    tr3: (G3 => G3 + mRNA3, α/(1+P2^n) + α0)
+    trl1: (mRNA1 => mRNA1 + P1, β * mRNA1)
+    trl2: (mRNA2 => mRNA2 + P2, β * mRNA2)
+    trl3: (mRNA3 => mRNA3 + P3, β * mRNA3)
+    d1: (mRNA1 => 0, mRNA1)
+    d2: (mRNA2 => 0, mRNA2)
+    d3: (mRNA3 => 0, mRNA3)
+    d4: (P1 => 0, P1)
+    d5: (P2 => 0, P2)
+    d6: (P3 => 0, P3)
 end "Repressilator pkg"
 
-set_observed_var!(repressilator, [:m1, :m2, :m3, :p1, :p2, :p3])
+set_observed_var!(repressilator, [:mRNA1, :mRNA2, :mRNA3, :P1, :P2, :P3])
+set_x0!(repressilator, [:mRNA1, :mRNA2, :mRNA3], fill(0, 3))
+set_x0!(repressilator, [:P1, :P2, :P3], [5, 0, 15])
+set_param!(repressilator, :n, 2.0)
+set_param!(repressilator, [:α, :α0, :β], [4400.0, 2.0, 4.0])
 set_time_bound!(repressilator, 15.0)
 
 export repressilator
