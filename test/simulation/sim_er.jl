9b40468e903ee38151e8abc97149eec320142f88
Mahmoud Bentriou <mahmoud.bentriou@centralesupelec.fr>
2022-05-15 13:09:47 +0200

    Fix error introduced by changing PyPlot to Plots

--- test/simulation/sim_er.jl
+++ test/simulation/sim_er.jl
@@ -6,7 +6,7 @@ load_model("ER")
 
 σ = simulate(ER)
 plot(times(σ), σ[:P], linetype=:steppost, linewidth=1.0)
-savefig(get_module_path() * "/test/simulation/res_pics/sim_er.png", dpi=480)
+savefig(get_module_path() * "/test/simulation/res_pics/sim_er.png")
 
 return true
 
--- test/simulation/sim_sir_col_buffer_bounded.jl
+++ test/simulation/sim_sir_col_buffer_bounded.jl
@@ -7,7 +7,7 @@ load_model("_bench_perf_test/SIR_col_buffer")
 SIR_col_buffer.time_bound = 100.0
 
 σ = _simulate_col_buffer(SIR_col_buffer)
-plot(times(σ), _get_values_row(σ,:I), linetype=:steppost, linewidth=1.0)
+plot(times(σ), _get_values_col(σ,:I), linetype=:steppost, linewidth=1.0)
 savefig(get_module_path() * "/test/simulation/res_pics/sim_sir_col_buffer_bounded.svg")
 
 return true
